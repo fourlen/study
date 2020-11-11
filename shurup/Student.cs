fb48e3ca2184375ab18bcf9d53e0287e530c2db2
fourlen <oborin.2013@mail.ru>
2020-11-11 23:27:24 +0500

    student

---
+++ shurup/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shurup
+{
+    class Student
+    {
+        public string firstname { get; set; }
+        public string middlename { get; set; }
+        public string lastname { get; set; }
+        public string group { get; set; }
+        public Dictionary<string, int> marks { get; set; }
+        public void printLong()
+        {
+            Console.WriteLine($"Фамилия: {lastname}\n" +
+                $"Имя: {firstname}\n" +
+                $"Отчество: {middlename}\n" +
+                $"Группа: {group}\n" +
+                $"Оценки: ");
+            subjects();
+        }
+        public void printShort()
+        {
+            Console.WriteLine($"{lastname} {firstname} {middlename} ({group})");
+        }
+        public void subjects()
+        {
+            foreach (KeyValuePair<string, int> pair in marks)
+            {
+                Console.WriteLine($"    {pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
---
+++ shurup/StudentsRegistry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shurup
+{
+    class StudentsRegistry
+    {
+        public List<Student> students { get; set; }
+        public StudentsRegistry instance { get; set; }
+        public StudentsRegistry() { }
+        public StudentsRegistry getInstance()
+        {
+            return instance;
+        }
+        public void addStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public void removeStudent(int n)
+        {
+            students.RemoveAt(n);
+        }
+        public void removeStudent(Student student)
+        {
+            students.Remove(student);
+        }
+        public Student GetStudent(int n)
+        {
+            return students[n];
+        }
+        public int getStudentCount()
+        {
+            return students.Count;
+        }
+    }
+}
