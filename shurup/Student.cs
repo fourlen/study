515f2749d1f96c45b27b4c54e852839f738ac0a3
fourlen <oborin.2013@mail.ru>
2020-11-15 21:48:21 +0500

    asdasd

--- shurup/Student.cs
+++ shurup/Student.cs
@@ -6,11 +6,13 @@ namespace shurup
 {
     class Student
     {
+        private static Student insnatce = null;
         public string firstname { get; set; }
         public string middlename { get; set; }
         public string lastname { get; set; }
         public string group { get; set; }
         public Dictionary<string, int> marks { get; set; }
+        private Student() { }
         public void printLong()
         {
             Console.WriteLine($"Фамилия: {lastname}\n" +
@@ -30,6 +32,14 @@ namespace shurup
             {
                 Console.WriteLine($"    {pair.Key}: {pair.Value}");
             }
+        }
+        public static Student GetInstance()
+        {
+            if (insnatce == null)
+            {
+                insnatce = new Student();
+            }
+            return insnatce;
         }
     }
 }
